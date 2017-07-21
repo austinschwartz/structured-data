b3179789c541796acd9ec214581fede3f2b5028f
nonis3 <nonis@purdue.edu>
2017-07-21 07:42:43 -0700

    56 passing, 75 failing

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,23 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [y (+ x x)]
+    (Math/pow y y)))
 
 (defn spiff [v]
-  :-)
+  (let [a (get v 0)]
+    (let [b (get v 2)]
+      (+ (if (nil? a) 0 a)
+         (if (nil? b) 0 b)))))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a _ b] v]
+    (let [b (get v 2)]
+      (+ (if (nil? a) 0 a)
+         (if (nil? b) 0 b)))))
 
 (defn point [x y]
   [x y])
@@ -19,22 +26,38 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle)
+     (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle)
+     (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[x3 y3] point]
+      (and
+        (<= x3 x2)
+        (<= y3 y2)
+        (>= x3 x1)
+        (>= y3 y1)))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer]
+  (let [[[x3 y3] [x4 y4]] inner]
+      (and
+        (>= x3 x1)
+        (>= y3 y1)
+        (<= x4 x2)
+        (<= y4 y2)))))
 
 (defn title-length [book]
   :-)
